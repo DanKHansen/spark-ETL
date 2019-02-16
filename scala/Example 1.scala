6c22bc10d22b23dd928e4301169ff8f97bb502e7
Dan K. Hansen <dankh@live.dk>
2019-02-16 22:25:48 +0100

    -

--- scala/Example 1.scala
+++ scala/Example 1.scala
@@ -10,7 +10,7 @@ object example {
 		val sparkConf = new SparkConf()                                         //create a new spark config
 		val sc = new SparkContext(sparkConf)                                    //build a spark context
 		 
-		sc.binaryFiles("/user/example/zip_dir", 10)                             //make an RDD from *.zip files in HDFS
+		sc.binaryFiles("C:\\temp\\test.zip", 10)                             //make an RDD from *.zip files in HDFS
 			.flatMap((file: (String, PortableDataStream)) => {                  //flatmap to unzip each file
 				val zipStream = new ZipInputStream(file._2.open)                //open a java.util.zip.ZipInputStream
 				val entry = zipStream.getNextEntry                              //get the first entry in the stream
@@ -18,6 +18,6 @@ object example {
 				iter.next                                                       //pop off the iterator's first line
 				iter                                                            //return the iterator
 			})
-			.saveAsTextFile("/user/example/quoteTable_csv/result.csv")          //save the RDD to a text file in HDFS
+			.saveAsTextFile("C:\\temp\\result.csv")          //save the RDD to a text file in HDFS
 	}
 }
